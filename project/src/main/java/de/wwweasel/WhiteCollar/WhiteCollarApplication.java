5abb8b42e23410bc5d98736debfaac8e05f5d30e
workurruti <weasel@tutamail.com>
2020-04-15 09:40:38 +0200

    init_commit

---
+++ project/src/main/java/de/wwweasel/WhiteCollar/WhiteCollarApplication.java
@@ -0,0 +1,13 @@
+package de.wwweasel.WhiteCollar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteCollarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteCollarApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/de/wwweasel/WhiteCollar/WhiteCollarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wwweasel.WhiteCollar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteCollarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
